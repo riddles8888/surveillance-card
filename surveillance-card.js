a8554bd0b06eebf4bd6abedb8ee1d699285de8a8
RiDdLeS <hyde.brandon@gmail.com>
2024-12-04 20:24:20 -0500

    Update surveillance-card.js

--- surveillance-card.js
+++ surveillance-card.js
@@ -368,5 +368,27 @@ class SurveillanceCard extends LitElement {
       }
     `;
   }
+  throttle(callback, delay) {
+    let isThrottled = false,
+      args,
+      context;
+    function wrapper() {
+      if (isThrottled) {
+        args = arguments;
+        context = this;
+        return;
+      }
+      isThrottled = true;
+      callback.apply(this, arguments);
+      setTimeout(() => {
+        isThrottled = false;
+        if (args) {
+          wrapper.apply(context, args);
+          args = context = null;
+        }
+      }, delay);
+    }
+    return wrapper;
+  }
 }
 customElements.define("surveillance-card", SurveillanceCard);
